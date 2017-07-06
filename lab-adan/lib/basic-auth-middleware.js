ae7f06281a3eb061a2942333d887ef93fbbd0344
adan.rabadan <evilb4k4@gmail.com>
2017-07-06 08:49:45 -0700

    initial commit and complete lab 16

---
+++ lab-adan/lib/basic-auth-middleware.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const User = require('../model/user.js');
+
+module.exports = (req, res, next) => {
+  const {authorization} = req.headers;
+
+  if(!authorization)
+    return next(new Error('unauthorized no authorization provieded'));
+
+  let encoded = authorization.split('Basic ')[1];
+  if(!encoded)
+    return next(new Error('unauthorized no basic auth provieded'));
+
+  let decoded = new Buffer(encoded, 'base64').toString();
+  let [username, password] = decoded.split(':');
+
+  if(!username || !password)
+    return next(new Error('unauthorized username or password was missing'));
+
+  console.log('decoded', decoded);
+  console.log('username', username);
+  console.log('password', password);
+
+  User.findOne({username})
+  .then(user => {
+    if(!user) return next(new Error('unauthorized user does not exist'));
+    return user.passwordHashCompare(password);
+  })
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => {
+    console.log('errrrrrrr', err);
+    next(new Error('unauthorized find one failed in basic auth middleware'));
+  });
+};
---
+++ lab-adan/lib/error-middleware.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = (err, req, res, next) => { //eslint-disable-line
+  console.error(err.message);
+
+  if (err.message.toLowerCase().includes('validation failed'))
+    return res.sendStatus(400);
+
+  if (err.message.toLowerCase().includes('request body'))
+    return res.sendStatus(400);
+
+  if (err.message.toLowerCase().includes('duplicate key'))
+    return res.sendStatus(409);
+
+  if (err.message.toLowerCase().includes('objectid failed'))
+    return res.sendStatus(404);
+
+  if (err.message.toLowerCase().includes('unauthorized'))
+    return res.sendStatus(401);
+
+  res.sendStatus(500);
+};
